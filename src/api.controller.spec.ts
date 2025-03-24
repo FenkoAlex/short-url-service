3002a5cebbd16aa4d8423a784904f59afaeb198a
Fenko Alex <fenkoalex@gmail.com>
2025-03-24 18:27:21 +0700

    add tests

--- src/api.controller.spec.ts
+++ src/api.controller.spec.ts
@@ -1,22 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ApiController } from './contrellers/api.controller';
-import { AppService } from './services/api.service';
+import * as request from 'supertest';
+import { v4 as uuid } from 'uuid';
 
-describe('AppController', () => {
-  let appController: ApiController;
+import { ApiController } from './contrellers/api.controller';
+import { ApiService } from './services/api.service';
+import { drizzleProvider } from './drizzle.provider';
+
+import type { INestApplication } from '@nestjs/common';
+
+const alias = uuid().slice(0, 8);
+const originalUrl = 'http://127.0.0.1:3000/test';
+
+describe('ApiController', () => {
+  let apiController: ApiController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [ApiController],
-      providers: [AppService],
+      providers: [ApiService, ...drizzleProvider],
     }).compile();
 
-    appController = app.get<ApiController>(ApiController);
+    apiController = app.get<ApiController>(ApiController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return short url', async () => {
+      expect(
+        await apiController.shorten({
+          originalUrl: originalUrl,
+          alias: alias,
+        }),
+      ).toBe(`${process.env.SERVICE_URL}/${alias}`);
     });
+  });
+});
+
+describe('app', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [ApiController],
+      providers: [ApiService, ...drizzleProvider],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`redirect`, () => {
+    const test = request(app.getHttpServer())
+      .get(`/v1/${alias}`)
+      .expect('Location', originalUrl);
+    return test;
   });
 });
